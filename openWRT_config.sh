fb43a1fcc5171dc9abfa0fa3a8d3537cdd7dfd04
silent-mobius <silent-mobius@users.noreply.github.com>
2015-05-16 16:23:56 +0300

    Update openWRT_config.sh
    initial commit

--- openWRT_config.sh
+++ openWRT_config.sh
@@ -9,33 +9,112 @@
 Reboot=0
 
 ###Funcs/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-do_install prerequisites(){
-	opkg install whiptail fdisk
+install_prerequisites(){
+	netTest=$(ping -c 1 >> /dev/null;echo $?)
+	if  [ "$netTest" == "0"];then 
+		opkg install whiptail fdisk
+	else
+		whiptail --msgbox " network is not available |script will not work as needed --> exiting " 20 60 2
+		return 0
+	fi
 	}
 	
-do_calc_wdth_hght(){
+wdth_hght(){
 	
 	}
 
-do_expandFS(){
+expandFS(){
+	if ! [ -h /dev/root ]; then
+		whiptail --msgbox "/dev/root does not exist or is not a symlink. Don't know how to expand" 20 60 2
+		return 0
+	fi
+		ROOT_PART=$(readlink /dev/root)
+		PART_NUM=${ROOT_PART#sda}
+	if [ "$PART_NUM" = "$ROOT_PART" ]; then
+		whiptail --msgbox "/dev/root is not an SD card. Don't know how to expand" 20 60 2
+		return 0
+	fi
+# NOTE: the NOOBS partition layout confuses parted. For now, let's only
+# agree to work with a sufficiently simple partition layout
+
+	if [ "$PART_NUM" -ne 2 ]; then
+		whiptail --msgbox "Your partition layout is not currently supported by this tool. You are probably using NOOBS, in which case your root filesystem is already expanded anyway." 20 60 2
+		return 0
+	fi
 	
+	LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+	if [ "$LAST_PART_NUM" != "$PART_NUM" ]; then
+		whiptail --msgbox "/dev/root is not the last partition. Don't know how to expand" 20 60 2
+		return 0
+	fi
+# Get the starting offset of the root partition
+PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d:)
+[ "$PART_START" ] || return 1
+# Return value will likely be error for fdisk as it fails to reload the
+# partition table because the root fs is mounted
+fdisk /dev/sda <<EOF
+p
+d
+$PART_NUM
+n
+p
+$PART_NUM
+$PART_START
+p
+w
+EOF
+ASK_TO_REBOOT=1
+# now set up an init.d script
+cat <<\EOF > /etc/init.d/resize2fs_once &&
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: resize2fs_once
+# Required-Start:
+# Required-Stop:
+# Default-Start: 2 3 4 5 S
+# Default-Stop:
+# Short-Description: Resize the root filesystem to fill partition
+# Description:
+### END INIT INFO
+. /lib/lsb/init-functions
+case "$1" in
+start)
+log_daemon_msg "Starting resize2fs_once" &&
+resize2fs /dev/root &&
+rm /etc/init.d/resize2fs_once &&
+update-rc.d resize2fs_once remove &&
+log_end_msg $?
+;;
+*)
+echo "Usage: $0 start" >&2
+exit 3
+;;
+esac
+EOF
+chmod +x /etc/init.d/resize2fs_once &&
+update-rc.d resize2fs_once defaults &&
+if [ "$INTERACTIVE" = True ]; then
+whiptail --msgbox "Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot" 20 60 2
+fi
 	}
 
-do_changePaswd(){ ##function used to change root passwd
+changePaswd(){ ##function used to change root passwd
 	whiptail --msgbox "You will now be asked to enter a new password for the pi user" 20 60 1
 		passwd  &&
 			whiptail --msgbox "Password changed successfully" 20 60 1
 	}
 	
-do_webInterFaceChange(){
+webInterFaceChange(){
 	
 	}
 
-do_overClock(){
-	
+overClock(){
+	whiptail --msgbox " WARNING!!!! ensure that you have good cooling system on the cpu or it might burn the cpu " 20 60 1
+			nvram set clkfreq=
+			nvram commit
 	}
 
-do_about(){
+about(){
 	
 	}
 
@@ -43,3 +122,4 @@ do_about(){
 #Main-_ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ -
 ###
 
+install_prerequisites
