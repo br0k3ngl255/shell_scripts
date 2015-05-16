2deae767040a16368ab292a10f7dda2723955588
silent-mobius <silent-mobius@users.noreply.github.com>
2015-05-16 15:10:46 +0300

    Create openWRT_config.sh
    initial commit

---
+++ openWRT_config.sh
@@ -0,0 +1,31 @@
+#!/bin/ash
+
+#######################################################################
+#
+#
+#
+#######################################################################
+
+###Vars +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Reboot=0
+
+###Funcs/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+do_install prerequisites(){
+	opkg install whiptail 
+	}
+do_calc_wdth_hght(){}
+do_expandFS(){}
+
+do_changePaswd(){ ##function used to change root passwd
+	whiptail --msgbox "You will now be asked to enter a new password for the pi user" 20 60 1
+		passwd  &&
+			whiptail --msgbox "Password changed successfully" 20 60 1
+	}
+	
+do_webInterFaceChange(){}
+do_overClock(){}
+do_about(){}
+###
+#Main-_ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ - _ -
+###
