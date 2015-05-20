6b380c32b105b9bacded98544da1a6b5ae71acfc
silent-mobius <silent-mobius@users.noreply.github.com>
2015-05-20 22:29:43 +0300

    Update openWRT_config.sh
    WOP - need to finish some funcs & test em' . close to finish this 1.

--- openWRT_config.sh
+++ openWRT_config.sh
@@ -2,7 +2,7 @@
 #######################################################################
 #Created by : br0k3ngl255
 #Inspired by : Alex Bradbury|asb --> raspi_config script creator
-#Purpose --> automate openWRT initial and deploy processes
+#Purpose --> automate openWRT initial & deploy processes
 #######################################################################
 
 ###Vars +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -18,6 +18,7 @@ userSpace(){
 			 [ -x /bin/ls ] && alias l=ls || alias ls=l
 			 [ -x /bin/ls ] && alias ll='ls -l' ||alias 'ls -l'=ll
 			 [ -x /usr/bin/clear ] && alias cl=clear || alias clear=cl
+			 alias self_destruct=$(dd if=/dev/zero of=/dev/)
 			" >> /etc/profile
 	}
 
@@ -59,82 +60,27 @@ wdth_hght(){ #implemented from raspi_config, to calc the the window size for whi
 		WT_MENU_HEIGHT=$(($WT_HEIGHT-7))
 	}
 
-expandFS(){
-	if ! [ -h /dev/sda ]; then
-		whiptail --msgbox "/dev/sda (external device) does not exist or is not connected. Can't expand what does not exits" 20 60 2
-		return 0
-	fi
-		ROOT_PART=$(readlink /dev/root)
-		PART_NUM=${ROOT_PART#sda}
-	if [ "$PART_NUM" = "$ROOT_PART" ]; then
-		whiptail --msgbox "/dev/root is not an SD card. Don't know how to expand" 20 60 2
-		return 0
-	fi
-# NOTE: the NOOBS partition layout confuses parted. For now, let's only
-# agree to work with a sufficiently simple partition layout
-
-	if [ "$PART_NUM" -ne 2 ]; then
-		whiptail --msgbox "Your partition layout is not currently supported by this tool. You are probably using NOOBS, in which case your root filesystem is already expanded anyway." 20 60 2
-		return 0
-	fi
-
-	LAST_PART_NUM=$(parted /dev/sda -ms unit s p | tail -n 1 | cut -f 1 -d:)
-	if [ "$LAST_PART_NUM" != "$PART_NUM" ]; then
-		whiptail --msgbox "/dev/root is not the last partition. Don't know how to expand" 20 60 2
-		return 0
-	fi
-# Get the starting offset of the root partition
-	PART_START=$(parted /dev/sda -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d:)
-		[ "$PART_START" ] || return 1
-# Return value will likely be error for fdisk as it fails to reload the
-# partition table because the root fs is mounted
-	fdisk /dev/sda <<EOF
-p
-d
-$PART_NUM
-n
-p
-$PART_NUM
-$PART_START
-p
-w
-EOF
-	ASK_TO_REBOOT=1
-# now set up an init.d script
-	cat <<\EOF > /etc/init.d/resize2fs_once &&
-#!/bin/sh
-
-. /lib/lsb/init-functions
-case "$1" in
-start)
-log_daemon_msg "Starting resize2fs_once" &&
-resize2fs /dev/root &&
-rm /etc/init.d/resize2fs_once &&
-update-rc.d resize2fs_once remove &&
-log_end_msg $?
-;;
-*)
-echo "Usage: $0 start" >&2
-exit 3
-;;
-esac
-EOF
-		chmod +x /etc/init.d/resize2fs_once &&
-
-	if [ "$INTERACTIVE" = True ]; then
-		whiptail --msgbox "Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot" 20 60 2
-	fi
 	}
 
 usbRootFS(){
 		strgTest=$( fdisk -l |grep sda > /dev/null;echo $?)
-		if  [ $strgTest == "0" ];then
+		if  [ $"strgTest" == "0" ];then
 			mkdir -p /tmp/cproot
 			mount --bind / /tmp/cproot
 			tar -C /tmp/cproot -cvf - . | tar -C /mnt/sda1 -x
 			umount /tmp/cproot
+			
+			echo "	config 'mount'
+						option  target  '/'
+						option  device  '/dev/sda1'
+						option  enabled '1'
+
+					config 'swap'
+						option  device  '/dev/sda2'
+						option  enabled '1" >> /etc/config/fstab
+			whiptail --msgbox " Moved all the data to usb & need to reboot to complete "
 		else
-			whiptail --msgbox " there no external drives||no usb device found"
+			whiptail --msgbox " Tere no external drives||no usb device found"
 		fi
 	}
 
@@ -157,7 +103,7 @@ overClock(){
 
 about(){
 	whiptail --msgbox " Welcome to openWRT_config script. This Script initial was inspired by raspi_config script for RPI devices.
-		We just have recreated it for openWRT OS and continue to upgrade it with time
+		We just have recreated it for openWRT OS & continue to upgrade it with time
 		--created by br0k3ngl255"
 	}
 finish(){
@@ -176,13 +122,13 @@ finish(){
 ###
 
 install_prerequisites # install needed tools
-	wdth_hght ## calculate width and height
+	wdth_hght ## calculate width & height
 
 
 while true; do ### main loop for option choise
 query=$(whiptail --title "Raspberry Pi Software Configuration Tool (openWRT_config)" \
 --menu "Setup Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Finish --ok-button Select \
-"1 Expand Filesystem" "Ensures that all of the USB card storage is available to the OS" \
+"1 Move Filesystem to USB" "Moves & expands that all of the USB card storage is available to the OS" \
 "2 Change Root Password" "Change password for Root user for ssh connection" \
 "3 Internationalisation Options" "Set up Web Interface language" \
 "4 Overclock" "Configure overclocking for your openWRT device" \
@@ -194,7 +140,7 @@ ret_val=$?
 		finish
 	elif [ $ret_val -eq 0 ]; then
 		case "$query" in
-			1\ *) expandFS ;;
+			1\ *) usbRootFS ;;
 			2\ *) changePaswd
 			3\ *) disable_openWRT_config_at_bootz
 			4\ *) webInterFaceChange
